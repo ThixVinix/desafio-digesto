b7d6abc804d7a5c8dafc1f4d9d03e2ec2fe3d77b
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-02 16:40:37 -0300

    Inclusão do componente bottom-navbar

---
+++ src/app/base/componentes/bottom-navbar/bottom-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-navbar',
+  templateUrl: './bottom-navbar.component.html',
+  styleUrls: ['./bottom-navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BottomNavbarComponent {
+
+}
