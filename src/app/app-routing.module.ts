11515b171c2fae0c3f4e7b8f15902090f8953f15
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-02 16:34:33 -0300

    Configuração da rota inicial do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplateBaseComponent } from './base/template-base/template-base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'buscarCnj', pathMatch: 'full' },
+  {
+    path: 'buscarCnj',
+    component: TemplateBaseComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'digesto-cnj' },
+      {
+        path: 'digesto-cnj',
+        loadChildren: () =>
+          import('./digesto-cnj/digesto-cnj.module').then(
+            (children) => children.DigestoCnjModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
