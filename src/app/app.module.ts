70f1b661475de8539c24a321724d173ea57f01e7
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-02 16:39:30 -0300

    Inclusão dos módulos iniciais e configuração da formatação textual do projeto para PT-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigestoCnjComponent } from './digesto-cnj/digesto-cnj.component';
+
+import { registerLocaleData } from '@angular/common';
+import LOCALE_PT from '@angular/common/locales/pt';
+import { BaseModule } from './base/base.module';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { DigestoCnjModule } from './digesto-cnj/digesto-cnj.module';
+
+registerLocaleData(LOCALE_PT);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BaseModule,
+    DigestoCnjModule,
   ],
-  providers: [],
+  providers: [
+    {
+      useValue: 'pt-BR',
+      provide: LOCALE_ID
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
