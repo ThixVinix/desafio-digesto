941ed663492258b7ff740966cf9b1d8b83c32d1a
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-02 16:44:04 -0300

    Inclusão do componente template-base

---
+++ src/app/base/template-base/template-base.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-base',
+  templateUrl: './template-base.component.html',
+  styleUrls: ['./template-base.component.scss']
+})
+export class TemplateBaseComponent {
+
+}
