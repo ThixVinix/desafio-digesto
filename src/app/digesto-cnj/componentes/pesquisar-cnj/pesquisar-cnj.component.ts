b11c90464f8818daa520ed9aed510329e03b134b
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-02 23:09:06 -0300

    Criação da lógica para validação do campo CNJ

--- src/app/digesto-cnj/componentes/pesquisar-cnj/pesquisar-cnj.component.ts
+++ src/app/digesto-cnj/componentes/pesquisar-cnj/pesquisar-cnj.component.ts
@@ -1,11 +1,68 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pesquisar-cnj',
   templateUrl: './pesquisar-cnj.component.html',
   styleUrls: ['./pesquisar-cnj.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PesquisarCnjComponent {
+export class PesquisarCnjComponent implements OnInit {
+  ngOnInit(): void {
+    this.isCnjValido = true;
+  }
 
+  @ViewChild('inputCnj', { static: false }) inputCnj!: ElementRef;
+
+  REGEX_CNJ = /^(\d{7})-(\d{2}).(\d{4}).(\d{1}).(\d{2}).(\d{4})$/;
+
+  MSG_FORMATO_INVALIDO: string =
+    "CNJ inválido, por favor inserir um CNJ no seguinte formato: 'XXXXXXX-XX.XXXX.X.XX.XXXX'";
+
+  mensagemErro: string = this.MSG_FORMATO_INVALIDO;
+
+  cnj: string = '';
+
+  isCnjValido: boolean = true;
+
+  constructor(private element: ElementRef, private render: Renderer2) {}
+
+  enviarCnj(): void {
+    if (!this.validarCnj(this.cnj)) {
+      alert(this.MSG_FORMATO_INVALIDO);
+      return;
+    } else {
+      alert('CNJ válido!');
+    }
+  }
+
+  checarPreenchimentoCnj(event: any): void {
+    let cnjInput: string = event.target.value;
+
+    if (!this.validarCnj(cnjInput)) {
+      this.isCnjValido = false;
+      this.mensagemErro = this.MSG_FORMATO_INVALIDO;
+      this.render.addClass(this.inputCnj.nativeElement, "is-invalid");
+      this.render.removeClass(this.inputCnj.nativeElement, "is-valid");
+    } else {
+      this.isCnjValido = true;
+      this.mensagemErro = '';
+      this.render.addClass(this.inputCnj.nativeElement, "is-valid");
+      this.render.removeClass(this.inputCnj.nativeElement, "is-invalid");
+    }
+  }
+
+  private validarCnj(cnj: string): boolean {
+    if (cnj == null || cnj == undefined || !cnj.match(this.REGEX_CNJ)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
