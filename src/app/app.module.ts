b3558839b0d466241778b72ae16bd741ae3fcee9
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-05 19:32:41 -0300

    Remoção de imports não utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DigestoCnjComponent } from './digesto-cnj/digesto-cnj.component';
 
 import { registerLocaleData } from '@angular/common';
 import LOCALE_PT from '@angular/common/locales/pt';
