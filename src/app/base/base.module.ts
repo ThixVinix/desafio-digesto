3c3ca4968c97a71dfc551d82c6f6c28c75c473c8
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-05 23:00:24 -0300

    Configuração dos provedores para carregamento e autorização da API

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,23 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TopNavbarComponent } from './componentes/top-navbar/top-navbar.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { BottomNavbarComponent } from './componentes/bottom-navbar/bottom-navbar.component';
 import { TemplateBaseComponent } from './template-base/template-base.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
+import { LoaderComponent } from './componentes/loader/loader.component';
+import { TopNavbarComponent } from './componentes/top-navbar/top-navbar.component';
+import { AuthorizationInterceptor } from './interceptadores/authorization.interceptor';
+import { LoaderInterceptor } from './interceptadores/loader.interceptor';
 
 
 @NgModule({
   declarations: [
     TemplateBaseComponent,
     TopNavbarComponent,
-    BottomNavbarComponent
+    BottomNavbarComponent,
+    LoaderComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule
-  ]
+  imports: [CommonModule, RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizationInterceptor,
+      multi: true,
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
+  exports: [LoaderComponent],
 })
 export class BaseModule { }
