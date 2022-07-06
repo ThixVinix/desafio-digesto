b36a495072a8899c96c9ddb25dc9bfe70389a1ff
Thiago Vinícius de Almeida Souza <tvasouza@hotmail.com>
2022-07-05 23:05:12 -0300

    Implementação da chamada ao serviço da API do Digesto

--- src/app/digesto-cnj/digesto-cnj.component.spec.ts
+++ src/app/digesto-cnj/digesto-cnj.component.spec.ts
@@ -1,6 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { DigestoCnjComponent } from './digesto-cnj.component';
+import { cnjMock } from './mockups/cnj.mock';
+import { CnjService } from './servicos/cnj.service';
 
 describe('DigestoCnjComponent', () => {
   let component: DigestoCnjComponent;
@@ -8,10 +12,13 @@ describe('DigestoCnjComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DigestoCnjComponent ]
-    })
-    .compileComponents();
+      declarations: [DigestoCnjComponent],
+      providers: [CnjService],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(DigestoCnjComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +26,14 @@ describe('DigestoCnjComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should getCnj', () => {
+    spyOn(component['cnjService'], 'getProcess').and.returnValue(of(cnjMock));
+    component.getCnj('5001682-88.2020.8.13.0672');
+
+    component.process$.subscribe((process) => {
+      expect(process).toEqual(cnjMock);
+    });
   });
 });
--- src/app/digesto-cnj/digesto-cnj.component.ts
+++ src/app/digesto-cnj/digesto-cnj.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { CnjInterface } from './interfaces/cnj.interface';
+import { CnjService } from './servicos/cnj.service';
 @Component({
   selector: 'app-digesto-cnj',
   templateUrl: './digesto-cnj.component.html',
   styleUrls: ['./digesto-cnj.component.scss']
 })
 export class DigestoCnjComponent {
+  process$!: Observable<CnjInterface>;
 
+  constructor(private cnjService: CnjService) {}
+
+  getCnj(cnj: string): void {
+    this.process$ = this.cnjService.getProcess(cnj);
+  }
 }
